20f7a4c6484f7d44650bb348696ea9ad634f577d
Yc.Shen <onixie@gmail.com>
2020-12-31 17:34:59 +0900

    Add asAddon to support labeling resource as addon

--- src/lib.nix
+++ src/lib.nix
@@ -5,4 +5,15 @@ with import <nixpkgs> {};
       runCommand "fromYAML" {} "cat '${yaml}' | ${pkgs.yaml2json}/bin/yaml2json > $out"
     )
   );
+  asAddon = with lib; mapAttrsRecursiveCond
+  (as: as ? "metadata" && as ? "apiVersion" && as ? "kind")
+  (ps: vs:
+  if !(last ps == "metadata" && isAttrs vs)
+  then vs
+  else mergeAttrs vs {
+    labels = {
+      "kubernetes.io/cluster-service"   = "true";
+      "addonmanager.kubernetes.io/mode" = "Reconcile";
+    };
+  });
 }
