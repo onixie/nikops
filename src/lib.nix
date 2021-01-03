a69e76957193add94909b7a66b59ae2e697bf9af
Yc.Shen <onixie@gmail.com>
2021-01-03 21:21:28 +0900

    Allow multi-doc yaml convertion and fix asAddon

--- src/lib.nix
+++ src/lib.nix
@@ -1,19 +1,67 @@
 with import <nixpkgs> {};
-{
-  fromYAML = yaml: builtins.fromJSON (
-    builtins.readFile (
-      runCommand "fromYAML" {} "cat '${yaml}' | ${pkgs.yaml2json}/bin/yaml2json > $out"
-    )
-  );
-  asAddon = with lib; mapAttrsRecursiveCond
-  (as: as ? "metadata" && as ? "apiVersion" && as ? "kind")
-  (ps: vs:
-  if !(last ps == "metadata" && isAttrs vs)
+rec {
+  fromYAML = yaml: {
+    apiVersion = "v1";
+    kind = "List";
+    items = builtins.fromJSON (
+      builtins.readFile (
+        runCommand "fromYAML" { buildInputs = [ jq yq ]; } "cat '${yaml}' | ${pkgs.yq}/bin/yq -sM . > $out"
+      )
+    );
+  };
+  asAddon = vs: with lib;
+  if isList vs
+  then map asAddon vs
+  else if !isAttrs vs
   then vs
-  else mergeAttrs vs {
-    labels = {
+  else let vs' = mapAttrs (ns: vs: asAddon vs) vs; in if !(vs ? "apiVersion" && vs ? "kind")
+  then vs'
+  else recursiveUpdate vs' {
+    metadata.labels = {
       "kubernetes.io/cluster-service"   = "true";
       "addonmanager.kubernetes.io/mode" = "Reconcile";
     };
-  });
+  };
+  enableAddonManagerIn = ns:
+  # code segment copied from
+  # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/services/cluster/kubernetes/addon-manager.nix#L96-L125
+  let
+    name = "system:kube-addon-manager";
+    namespace = ns;
+  in
+  {
+    apiVersion = "v1";
+    kind = "List";
+    items = [
+      {
+        apiVersion = "rbac.authorization.k8s.io/v1";
+        kind = "Role";
+        metadata = {
+          inherit name namespace;
+        };
+        rules = [{
+          apiGroups = ["*"];
+          resources = ["*"];
+          verbs = ["*"];
+        }];
+      }
+      {
+        apiVersion = "rbac.authorization.k8s.io/v1";
+        kind = "RoleBinding";
+        metadata = {
+          inherit name namespace;
+        };
+        roleRef = {
+          apiGroup = "rbac.authorization.k8s.io";
+          kind = "Role";
+          inherit name;
+        };
+        subjects = [{
+          apiGroup = "rbac.authorization.k8s.io";
+          kind = "User";
+          inherit name;
+        }];
+      }
+    ];
+  };
 }
